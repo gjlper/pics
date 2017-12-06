da2696c8859dea1acaf70c64b7b6b472458c59e4
xucheng.zhu@berkeley.edu <xucheng.zhu@berkeley.edu>
2017-12-06 15:24:29 -0800

    TGV update

--- @TGV/mtimes.m
+++ @TGV/mtimes.m
@@ -2,8 +2,8 @@ function res = mtimes(a,b)
 % TGV prox operator
 
 if a.params.nflag
-    b_n = b./max(abs(b)+eps);
-    b_scale = max(abs(b)+eps);
+    b_n = b./max(abs(b(:))+eps);
+    b_scale = max(abs(b(:))+eps);
 else
     b_n = b;
     b_scale = 1;
@@ -12,12 +12,13 @@ end
 u_b = b_n;
 v = pgradient(b_n,4);
 v_b = v;
-sigma = 0.05;
-tau = 0.1;
-alpha0 = 0.1;
-alpha1 = 0.05;
-p = zeros(size(b_n));
-q = zeros(size(v));
+lambda = a.params.lambda;%0.02
+sigma = a.params.sigma;%.05
+tau = a.params.tau;%.1
+alpha0 = a.params.alpha0;%.1
+alpha1 = a.params.alpha1;%.05
+p = zeros([size(b_n),3]);
+q = zeros([size(v),3]);
 % interation??
 Iter = 5;
 for i = 1:Iter
@@ -25,9 +26,9 @@ for i = 1:Iter
     p = prox_linf2(p+sigma*(pgradient(u_b,4)-v_b),alpha0,4);
     q = prox_linf2(q+sigma*(pgradient(v_b,5)),alpha1,[4,5]);
     % primal update
-    u_o = b_n;
-    u = (lambda*(b_n + div4(p)) + tau*I_in)/(lambda+tau);
-    u_b = u + (u - u_o);
+    u_o = u_b;
+    u_b = (lambda*(u_o + div4(p)) + tau*b_n)/(lambda+tau);
+    u_b = u_b + (u_b - u_o);
     v_o = v;
     v = v + (p + div5(q));
     v_b = v + (v - v_o);
