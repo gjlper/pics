ef72b1767463f444b7f86026dc82941a0d79e994
Xucheng Zhu <xzhu@dublin.radiology.ucsf.edu>
2018-03-12 14:54:31 -0700

    ADD:modify mask function

--- util/mask_ball.m
+++ util/mask_ball.m
@@ -1,13 +1,26 @@
-function mask = mask_ball(msize,rsize)
+function mask = mask_ball(msize,rsize,s_flag)
 % ball shape mask
 % input:
 %   msize: matrix size
 %   rsize: radius of ball
+%   s_flag: soft mask flag
 % output:
 %   mask:  ball mask
 
-ax = (1:msize(1))-floor((msize(1)+1)/2);
-ay = (1:msize(2))-floor((msize(2)+1)/2);
-az = (1:msize(3))-floor((msize(3)+1)/2);
+rsize = abs(rsize);
+ax = (1:msize(1))-round((msize(1)+1)/2);
+ay = (1:msize(2))-round((msize(2)+1)/2);
+az = (1:msize(3))-round((msize(3)+1)/2);
+    
 [my,mx,mz] = meshgrid(ay,ax,az);
-mask = (sqrt(mx.^2+my.^2+mz.^2)<=rsize);
+
+if ~isempty(s_flag)
+    if s_flag
+        % mask = exp(-sqrt(mx.^2+my.^2+mz.^2)/rsize);
+        mask = rsize ./ (sqrt(mx.^2+my.^2+mz.^2) + rsize.^2 + eps);
+    else
+        mask = (sqrt(mx.^2+my.^2+mz.^2)<=rsize);
+    end
+else
+    mask = (sqrt(mx.^2+my.^2+mz.^2)<=rsize);
+end
