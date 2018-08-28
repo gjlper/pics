69846dc67933555498e0798131134cbb026d7848
xucheng.zhu@berkeley.edu <xucheng.zhu@berkeley.edu>
2018-08-28 16:58:56 -0500

    ADD qsm scipt

---
+++ QSM_script.m
@@ -0,0 +1,14 @@
+% QSM script
+% B0 = 3;
+% H = [0,0,1];
+
+% load multi-echo data [nx,ny,nz,ncoil]
+% I =
+
+% Mask for soft tissue
+% tmask = 
+% dTE =, TE0=, or define TE array
+
+% raw_phase = 
+% [Tissue_Phase, Tissue_Mask, bkg_phase] = VSHARP(raw_phase,tmask,12);
+% QSM = QSM_solver(Tissue_Phase=, Tissue_Mask, dTE, B0);
