2a5fcec8ecd7bf614fadac65a8ede688fe82839c
xucheng.zhu@berkeley.edu <xucheng.zhu@berkeley.edu>
2018-08-29 00:26:07 -0500

    UPDATE TGV files

--- @TGV/mtimes.m
+++ @TGV/mtimes.m
@@ -26,9 +26,9 @@ for i = 1:Iter
     p = prox_linf2(p+sigma*(pgradient(u_b,4)-v_b),alpha0,4);
     q = prox_linf2(q+sigma*(pgradient(v_b,5)),alpha1,[4,5]);
     % primal update
-    u_o = u_b;
-    u_b = (lambda*(u_o + tau*div4(p)) + tau*b_n)/(lambda+tau);
-    u_b = u_b + (u_b - u_o);
+    u_o = u;
+    u = (lambda*(u_o + tau*div4(p)) + tau*b_n)/(lambda+tau);
+    u_b = u + (u - u_o);
     v_o = v;
     v = v + tau*(p + div5(q));
     v_b = v + (v - v_o);
